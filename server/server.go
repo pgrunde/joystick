69fb9447f2edbfe0144f275e67e4ddc518a251db
pgrunde <peter.a.grunde@gmail.com>
2019-10-26 23:30:24 -0600

    gorilla added, basic websocket with no client

--- server/server.go
+++ server/server.go
@@ -5,18 +5,21 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/pgrunde/joystick/templating"
 )
 
 type App struct {
 	mux       *http.ServeMux
 	templates *templating.Templates
+	upgrader  websocket.Upgrader
 	port      int16
 }
 
 func (a *App) ListenAndServe() error {
 	fileServer := http.FileServer(http.Dir("./dist"))
 	a.mux.Handle("/dist/", http.StripPrefix("/dist/", fileServer))
+	a.mux.HandleFunc("/ws", app.joystickSocket)
 	log.Printf(
 		"Starting server on port '%d'",
 		a.port,
@@ -31,9 +34,10 @@ func New() *App {
 	port := int16(3000)
 	locals := templating.Attrs{}
 	app := App{
-		port:      port,
 		mux:       http.NewServeMux(),
 		templates: templating.TemplatesFromDir("./templating/templates", locals),
+		upgrader:  websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024},
+		port:      port,
 	}
 	app.mux.HandleFunc("/", app.root)
 	return &app
@@ -41,4 +45,30 @@ func New() *App {
 
 func (a *App) root(w http.ResponseWriter, r *http.Request) {
 	a.templates.Execute(w, "joystick")
+}
+
+func (a *App) joystickSocket(w http.ResponseWriter, r *http.Request) {
+	// TODO check CORS if you ever open to actual internet
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	wsConn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	reader(ws)
+}
+
+// https://tutorialedge.net/golang/go-websocket-tutorial/
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println(string(p))
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
